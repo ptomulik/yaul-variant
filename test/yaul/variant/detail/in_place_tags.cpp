8ec1a21986d4346c0c519b26953448692f709b5a
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2017-03-02 09:46:08 +0100

    update copyright dates

--- test/yaul/variant/detail/in_place_tags.cpp
+++ test/yaul/variant/detail/in_place_tags.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2015, Pawel Tomulik <ptomulik@meil.pw.edu.pl>
+// Copyright (C) 2015-2017, Pawel Tomulik <ptomulik@meil.pw.edu.pl>
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
