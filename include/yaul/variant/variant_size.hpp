9f0313e5186781622b52cf4991d7158b7a5a18fe
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2017-03-04 23:19:05 +0100

    added variant_size.hpp

---
+++ include/yaul/variant/variant_size.hpp
@@ -0,0 +1,69 @@
+// Copyright (C) 2015, Pawel Tomulik <ptomulik@meil.pw.edu.pl>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+// yaul/variant/variant_size.hpp
+
+/** // doc: yaul/variant/variant_size.hpp {{{
+ * \file yaul/variant/variant_size.hpp
+ * \brief Implements \ref yaul::variant_size metafunction
+ */ // }}}
+#ifndef YAUL_VARIANT_VARIANT_SIZE_HPP
+#define YAUL_VARIANT_VARIANT_SIZE_HPP
+
+#include <yaul/variant/variant_fwd.hpp>
+#include <type_traits>
+#include <cstddef>
+
+namespace yaul {
+
+/** // doc: variant_size {{{
+ * \brief Provides access to the number of alternatives in a possibly
+ *        cv-qualified variant as a compile-time constant expression.
+ *
+ * \tparam Variant The variant type to be examined.
+ *
+ * \par Synopsis
+ * \code
+ *  template <typename T>
+ *  struct variant_size; // undefined
+ * \endcode
+ * \code
+ *  template <typename T0, typename... Others>
+ *  struct variant_size<variant<T0,Others...> >
+ *    : std::integral_constant<std::size_t, 1ul + sizeof.,..(Others)> { };
+ * \endcode
+ * \code
+ *  template <typename T> struct variant_size<T const>;
+ *  template <typename T> struct variant_size<T volatile>;
+ *  template <typename T> struct variant_size<T const volatile>;
+ * \endcode
+ */ // }}}
+template <typename Variant>
+  struct variant_size;
+/** \cond DOXYGEN_SHOW_TEMPLATE_SPECIALIZATIONS */
+template <typename T0, typename... Others>
+  struct variant_size<variant<T0, Others...> >
+    : std::integral_constant<std::size_t, 1ul + sizeof...(Others)>
+  { };
+template <typename T>
+  struct variant_size<T const>
+    : variant_size<T>
+  { };
+template <typename T>
+  struct variant_size<T volatile>
+    : variant_size<T>
+  { };
+template <typename T>
+  struct variant_size<T const volatile>
+    : variant_size<T>
+  { };
+/** \endcond */
+
+} // end namespace yaul::detail::variant
+
+#endif /* YAUL_VARIANT_VARIANT_SIZE_HPP */
+// vim: set expandtab tabstop=2 shiftwidth=2:
+// vim: set foldmethod=marker foldcolumn=4:
---
+++ test/yaul/variant/variant_size.cpp
@@ -0,0 +1,24 @@
+// Copyright (C) 2015, Pawel Tomulik <ptomulik@meil.pw.edu.pl>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <yaul/variant/variant_size.hpp>
+#include <type_traits>
+
+using yaul::variant_size;
+using yaul::variant;
+
+static_assert(variant_size<variant<int> >::value == 1ul, "");
+static_assert(variant_size<variant<int,char> >::value == 2ul, "");
+static_assert(variant_size<variant<int,char,float> >::value == 3ul, "");
+
+static_assert(variant_size<variant<int,char,float> const>::value == 3ul, "");
+static_assert(variant_size<variant<int,char,float> volatile>::value == 3ul, "");
+static_assert(variant_size<variant<int,char,float> const volatile>::value == 3ul, "");
+
+int main()
+{
+  return 0;
+}
